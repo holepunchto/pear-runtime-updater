59cf8400af6c5ce7d31e748e3da25d7c3f9bcea6
Jan Keith Darunday <jkcdarunday@gmail.com>
2026-02-27 19:24:41 +0800

    Install pear-runtime-updater and use code from hello-pear

--- test/fixtures/updater/index.js
+++ test/fixtures/updater/index.js
@@ -1,5 +1,13 @@
 const { app, BrowserWindow, ipcMain } = require('electron')
+const { isMac, isLinux } = require('which-runtime')
+const os = require('os')
+const path = require('path')
+const PearRuntime = require('pear-runtime')
+const pkg = require('./package.json')
+const { name, productName, version, upgrade } = pkg
 
+let pear
+const appName = productName ?? name
 const CI = !!process.env.CI
 if (CI) {
   app.commandLine.appendSwitch('headless')
@@ -44,3 +52,36 @@ app.on('window-all-closed', () => {
     app.quit()
   }
 })
+
+;(async () => {
+  console.log('running')
+  const pear = await getPear()
+  pear.updater.on('updating', function () { console.log('updating') })
+  pear.updater.on('updated', function () { console.log('updated') })
+
+  console.log('started', version, upgrade)
+})()
+
+async function getPear() {
+  if (pear) return pear
+  const appPath = getAppPath()
+  let dir = null
+  if (appPath === null) {
+    dir = path.join(os.tmpdir(), 'pear', appName)
+  } else {
+    dir = isMac
+      ? path.join(os.homedir(), 'Library', 'Application Support', appName)
+      : isLinux
+        ? path.join(os.homedir(), '.config', appName)
+        : path.join(os.homedir(), 'AppData', 'Roaming', appName)
+  }
+  const bootstrap = JSON.parse(process.env.PEAR_BOOTSTRAP || '[]')
+  pear = new PearRuntime({ dir, app: appPath, bootstrap, updates: true, version, upgrade })
+  return pear
+}
+
+function getAppPath() {
+  if (!app.isPackaged) return null
+  if (isLinux && process.env.APPIMAGE) return process.env.APPIMAGE
+  return path.join(process.resourcesPath, '..', '..')
+}
