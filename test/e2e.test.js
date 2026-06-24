f8635c3f8f5748ed4dee38d2cdbc009d227bedb1
Jan Keith Darunday <jkcdarunday@gmail.com>
2026-06-25 05:20:19 +0800

    Fix wrong usage of t.execution

--- test/e2e.test.js
+++ test/e2e.test.js
@@ -194,7 +194,7 @@ test('should receive and apply update when update happens while app is running',
   await t.execution(updated, 'got updated message')
 
   t.comment('wait for exit')
-  await t.execution(await exit, 'app exited successfully')
+  await t.execution(exit, 'app exited successfully')
 
   if (isWindows) {
     t.comment('give time for MSIX installer to finish')
@@ -224,7 +224,7 @@ test('should receive and apply update when update happens while app is running',
 
   t.is(await startedVersion, '1.0.1', 'version matches updated value (1.0.1)')
 
-  await t.execution(await exit, 'app exited successfully')
+  await t.execution(exit, 'app exited successfully')
 })
 
 test('should receive and apply update when update happens while app is not running', async (t) => {
@@ -372,7 +372,7 @@ test('should receive and apply update when update happens while app is not runni
   await t.execution(updated, 'got updated message')
 
   t.comment('wait for exit')
-  await t.execution(await exit, 'app exited successfully')
+  await t.execution(exit, 'app exited successfully')
 
   if (isWindows) {
     t.comment('give time for MSIX installer to finish')
@@ -402,7 +402,7 @@ test('should receive and apply update when update happens while app is not runni
 
   t.is(await startedVersion, '1.0.1', 'version matches updated value (1.0.1)')
 
-  await t.execution(await exit, 'app exited successfully')
+  await t.execution(exit, 'app exited successfully')
 })
 
 test('should update from prerelease to release', async (t) => {
@@ -578,7 +578,7 @@ test('should update from prerelease to release', async (t) => {
   await t.execution(updated, 'got updated message')
 
   t.comment('wait for exit')
-  await t.execution(await exit, 'app exited successfully')
+  await t.execution(exit, 'app exited successfully')
 
   if (isWindows) {
     t.comment('give time for MSIX installer to finish')
@@ -608,7 +608,7 @@ test('should update from prerelease to release', async (t) => {
 
   t.is(await startedVersion, '1.0.0', 'version matches updated value (1.0.0)')
 
-  await t.execution(await exit, 'app exited successfully')
+  await t.execution(exit, 'app exited successfully')
 })
 
 unhookTestnet('destroy testnet', async () => {
